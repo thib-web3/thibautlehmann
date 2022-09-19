4a4c21134a7b9d714454d27eb840d017ca622f1c
thiblehm12 <thiblehm12@gmail.com>
2022-09-19 19:08:37 +0200

    add link to contact me

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -3,13 +3,15 @@ import Image from 'next/image'
 import styles from '../../styles/components/navbar.module.css'
 import { BsLinkedin } from 'react-icons/bs'
 import ConnectWallet from './connectWallet'
-//
+import { FiArrowUpRight } from 'react-icons/fi'
+import splitbee from '@splitbee/web';
 const Navbar: NextPage = () => {
+    splitbee.init()
     return (
         <div className={styles.container}>
 
-            <a href="https://www.linkedin.com/in/thibaut-lehmann/" target="_blank" rel="noreferrer" className={styles.twitter}>
-                Contact me
+            <a href="https://www.linkedin.com/in/thibaut-lehmann/" target="_blank" rel="noreferrer" className={styles.contact}>
+                Contact me <FiArrowUpRight />
             </a>
             <div className={styles.buttons}>
                 <ConnectWallet />
