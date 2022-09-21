f13fbc47fe3e5390b8ed3c73dce5ac842f60931c
thiblehm12 <thiblehm12@gmail.com>
2022-09-21 08:52:56 +0200

    modified about section

--- components/about/about.tsx
+++ components/about/about.tsx
@@ -9,10 +9,20 @@ const About: NextPage = () => {
             <h2 className={styles.title}>
                 About
             </h2>
-            <p className={styles.description}>
-                I’m Thibaut, a 20 years old creative blockchain developer, from France. With two years of experience in web2, and one year in web3,
-                I’m able to think and create scalable web3 decentralized applications.
-            </p>
+            <div className={styles.section}>
+
+                <div className={styles.left}></div>
+                <div className={styles.right}>
+                    <p className={styles.description}>
+                        I’m Thibaut, a french 20 years old, creative developer passionated about blockchain.
+                    </p>
+                    <p className={styles.description}>
+                        With 2 years of experience in web2, 1 year in web3.</p>
+                    <p className={styles.description}>
+                        I’m able to think and create scalable web3 decentralized applications.</p>
+
+                </div>
+            </div>
         </div>
     )
 }
